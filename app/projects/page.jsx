13e2841e10253968176cc1694cbaf379cce5bc42
skyacinthus <salmakarimahkamal@gmail.com>
2025-10-21 18:25:24 +0700

    fix projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -3,7 +3,7 @@ export default function Projects() {
     <main className="flex flex-col items-center justify-center min-h-screen text-center p-8">
       <h1 className="text-4xl font-bold mb-4">Projects/Achievements</h1>
       <p className="max-w-xl text-gray-600">
-        text
+        some things ive built 
       </p>
     </main>
   );
