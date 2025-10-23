2625b29ed8c33de2cb19bf19e01887f5848f5e17
skyacinthus <salmakarimahkamal@gmail.com>
2025-10-23 21:33:28 +0700

    style home

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,11 +7,11 @@ export default function About() {
         src="/headshot_teal.png"  alt="Salma Kamal" width={200} height={200} className="object-cover rounded-full hover:shadow-2xl transition duration-300 mb-4"
       />
       <h1 className="text-4xl md:text-5xl font-bold mb-4 text-pink-900 dark:text-pink-300">About Me!</h1>
-      <p className="max-w-xl md:max-w-3xl text-lg mb-4 text-gray-600 dark:text-gray-300">
+      <p className="max-w-xs md:max-w-3xl md:text-lg mb-4 text-gray-600 dark:text-gray-300">
       Hi! I’m <span className="font-semibold">Salma Karimah Kamal</span>, an Information Engineering student at UGM, with a passion for design, technology, and creating practical solutions through both logic and creativity.
       </p>
-      <p className="max-w-screen md:max-w-3xl text-lg mb-4 text-gray-600 dark:text-gray-300"> 
-      I enjoy learning about how things are made — from small components in manufacturing to digital systems that improve everyday life. My background in robotics and my growing skills in programming have shape the way I approach problems: logically, creatively, and with curiosity.
+      <p className="max-w-xs md:max-w-3xl md:text-lg mb-4 text-gray-600 dark:text-gray-300"> 
+      I love learning how things work, from the logic behind code to the structure of real-world systems! I'm always eager to explore new ways to bridge creativity and engineering in my projects.
       </p>
     </main>
   );
--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -3,7 +3,7 @@ export default function contacts() {
     <main className="flex flex-col items-center mt-20 text-center justify-center">
         <h1 className="text-4xl md:text-5xl font-bold mb-4 text-pink-900 dark:text-pink-300">Contact Me!</h1>
         <p className="text-lg text-gray-600 dark:text-gray-200 font-bold mb-6 max-w-xl">
-          Let’s connect! Reach me through any of the following platforms:
+          Let's connect! Reach me through any of the following platforms:
         </p>
         <div className="bg-teal-800 rounded-2xl shadow-md p-6 text-left hover:shadow-xl transition duration-300 justify-center">
           <p>📧 <a href="mailto:salmakarimahkamal@gmail.com" className="text-white font-bold hover:underline">salmakarimahkamal@gmail.com</a></p>
