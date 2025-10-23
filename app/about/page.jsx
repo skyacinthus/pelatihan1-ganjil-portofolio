28db0e60e23665ec7e3e0a422340a11a508aaeac
skyacinthus <salmakarimahkamal@gmail.com>
2025-10-23 17:55:46 +0700

    working payload

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <Image
         src="/headshot_teal.png"  alt="Salma Kamal" width={200} height={200} className="object-cover rounded-full shadow-lg mb-4"
       />
-      <h1 className="text-4xl md:text-5xl font-bold mb-4">About Me!</h1>
+      <h1 className="text-4xl md:text-5xl font-bold mb-4 text-pink-900 dark:text-pink-200">About Me!</h1>
       <p className="max-w-screen md:max-w-xl text-gray-600">
         kjsdfkjadfjhaksdfha sdfhaksd fkjahsdfkjha sfahsdfjhsdhf ahdfkafda sfhskjdhfkha dkhajf ahsdfhkahsdfkhajsdhf asfdsdfadjkahjkhdfa sffa sdhfhakdfhkah dahdfkjh adjfhak
         hdhfkjajfh ashakfh dshhasdf  kafhasf 
